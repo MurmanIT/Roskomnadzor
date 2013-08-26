30a1eeefb290de3668ce4045ba3552cc82202b5e
MurmanIT <work@murmanit.ru>
2013-08-26 17:58:05 +0400

    soap_file

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-	
+$url         = "http://vigruzki.rkn.gov.ru/services/OperatorRequest/?wsdl"; 
+$cliente = new SoapClient($url);
+$sample_add = array("code" => "151914a30509a8c5391beaea50cdc03a");
+$vem = $cliente->__soapCall('getResult',array($sample_add));
+echo $vem->resultComment;
+print_r($vem);
 ?>
---
+++ list.php
@@ -0,0 +1,4 @@
+<?php
+	$sxe = file_get_contents("php://input");                                           file_put_contents("test.txt",$sxe);
+	
+?>
